c689f455e2e4175cfa3fb9fb1d8e73fdfb82ade5
VladRastorguev <vlad-rastorguev@list.ru>
2024-05-17 14:21:49 +0600

    Create1C fix

---
+++ src/Component/ContentPerMainPaig/Interface/CreateAccount1cInterface.ts
@@ -0,0 +1,12 @@
+export interface CreaetAccount1cInterface {
+    InterfaceObj: {
+        changeUserNameCreate1c: ((newState: string) => void) | null
+        changeUserCompanyCreate1c: ((newState: string) => void) | null
+        changeUserEmailCreate1c: ((newState: string) => void) | null
+        changeUserJobtitelCreate1c: ((newState: string) => void) | null
+        userName: string
+        userCompany: string
+        userEmail: string
+        userJobtitel: string
+    }
+}
